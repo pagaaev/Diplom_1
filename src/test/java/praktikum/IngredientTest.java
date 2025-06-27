a61b784d6cab956cd9e1f72bcbe4a6188529ea9b
Artur Pagaev <paagaev@mail.ru>
2025-06-27 09:59:52 +0700

    Разделил тесты IngredientTest на отдельные проверки

--- src/test/java/praktikum/IngredientTest.java
+++ src/test/java/praktikum/IngredientTest.java
@@ -6,10 +6,20 @@ import static org.junit.jupiter.api.Assertions.*;
 public class IngredientTest {
 
     @Test
-    public void testIngredientProperties() {
+    public void testGetType() {
         Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Hot Sauce", 99.9f);
         assertEquals(IngredientType.SAUCE, ingredient.getType());
+    }
+
+    @Test
+    public void testGetName() {
+        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Hot Sauce", 99.9f);
         assertEquals("Hot Sauce", ingredient.getName());
+    }
+
+    @Test
+    public void testGetPrice() {
+        Ingredient ingredient = new Ingredient(IngredientType.SAUCE, "Hot Sauce", 99.9f);
         assertEquals(99.9f, ingredient.getPrice());
     }
 }
